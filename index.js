13150bf79697783de0ee39ba02ee44feda86534b
HappyDay <dev@rewindgravity.com>
2020-05-02 17:16:20 +0200

    #2 Add nutrient and recipe model classes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const Ingredient = require('./src/models/ingredient');
+
+const myNewIngredient = new Ingredient("Haselnuss");
+
+
+
+console.log(myNewIngredient);
---
+++ src/models/nutrient.js
@@ -0,0 +1,9 @@
+class Nutrient {
+    constructor(name, value, unit) {
+        this.name = name;
+        this.value = value;
+        this.unit = unit;
+    }
+}
+
+module.exports = Nutrient
