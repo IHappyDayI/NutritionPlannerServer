6bd81e357773a8c70ad21e619c2cf2ab506e6eb9
HappyDay <dev@rewindgravity.com>
2020-05-05 23:23:26 +0200

    #1 Add ingredient to create_recipe migration and use a uuid instead of an integer

--- migrations/20200503011932_create_recipe.js
+++ migrations/20200503011932_create_recipe.js
@@ -1,8 +1,12 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('recipe', (table) => {
-        table.increments('id')
+        table
+            .uuid('id')
+            .notNullable()
+            .unique()
         table.string('name')
         table.string('description')
+        table.string('ingredient')
         table.string('workflow')
     })
 };
